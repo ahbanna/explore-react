b6fc9f7fda3197d1956457da8d3744389b057dbe
Al Hasanul Banna <tutul5800@gmail.com>
2023-03-24 16:15:23 +0600

    obj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const friends = ["Hasan", "Kamal", "Jamal", "Kabir", "Ariful"];
+  const products = [
+    {
+      name: "Iphone",
+      price: "80000",
+    },
+    {
+      name: "Laptop",
+      price: "50000",
+    },
+    {
+      name: "Watch",
+      price: "2000",
+    },
+  ];
+
+  const bestPlayer = {
+    name: "Shahid Afridi",
+    nationality: "Pakistan",
+    age: 40,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {friends.map((friend) => (
+        <Friend name={friend}></Friend>
+      ))}
+
+      {products.map((product) => (
+        <Product name={product.name} price={product.price}></Product>
+      ))}
+
+      <BestPlayer
+        name={bestPlayer.name}
+        nationality={bestPlayer.nationality}
+        age={bestPlayer.age}
+      ></BestPlayer>
+    </div>
+  );
+}
+
+// console.log();
+
+function Friend(props) {
+  return (
+    <div className="single-item">
+      <h3>Name: {props.name}</h3>
+      <p>{props.playerName}</p>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="single-product">
+      <h3>Produst Name: {props.name} </h3>
+      <p>Price: {props.price} </p>
+    </div>
+  );
+}
+
+function BestPlayer(props) {
+  return (
+    <div>
+      <h3>Best player Name: {props.name}</h3>
+      <p>Age: {props.age}</p>
+      <p>Nationality: {props.nationality}</p>
     </div>
   );
 }
